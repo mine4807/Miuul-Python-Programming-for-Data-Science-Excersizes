cddd2e4585fb56563d87a2e0443c7d652d9b05e2
mine <32925538+mine4807@users.noreply.github.com>
2024-05-28 14:36:32 +0300

    Create capitalize_even_indices.py

---
+++ capitalize_even_indices.py
@@ -0,0 +1,11 @@
+def string_cover(string):
+    new_str=""
+    for i in range(len(string)):
+        if i % 2 == 0:  #index çiftse
+            new_str+=string[i].upper()
+        else:
+            new_str+=string[i]
+    return new_str
+    
+print(string_cover("hi my name is mine"))
+            
